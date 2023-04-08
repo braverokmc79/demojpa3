9ae3c4b5090e074c3efd0897de7ac6c22782303d
myspringbootstyle <108319537+myspringbootstyle@users.noreply.github.com>
2023-04-09 08:19:36 +0900

    first commit

---
+++ src/main/java/com/example/demojap3/Demojap3Application.java
@@ -0,0 +1,14 @@
+package com.example.demojap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demojap3Application.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/demojap3/Demojap3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojap3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojap3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
