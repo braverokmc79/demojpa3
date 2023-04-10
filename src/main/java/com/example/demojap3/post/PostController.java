080833932b90544935b26e99c895dbbac8e0d1f3
myspringbootstyle <108319537+myspringbootstyle@users.noreply.github.com>
2023-04-10 16:09:51 +0900

    29.스프링 데이터 Common 13. 웹 기능 3부 Pageable과 Sort

--- src/main/java/com/example/demojap3/post/PostController.java
+++ src/main/java/com/example/demojap3/post/PostController.java
@@ -1,6 +1,8 @@
 package com.example.demojap3.post;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +23,17 @@ public class PostController {
     }
 
 
-    @GetMapping("/posts/{id}")
-    public String getAPost2(@PathVariable("id") Post post){
-        return  post.getTitle();
+//    @GetMapping("/posts/{id}")
+//    public String getAPost2(@PathVariable("id") Post post){
+//        return  post.getTitle();
+//    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable){
+        return postRepository.findAll(pageable);
     }
+
+
+
 
 }
