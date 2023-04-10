42dc34cb26c5b3bc617ba7faaa51eeeb8f1ba1b7
myspringbootstyle <108319537+myspringbootstyle@users.noreply.github.com>
2023-04-10 19:41:32 +0900

    32.스프링 데이터 JPA 2. JpaRepository.save() 메소드

--- src/test/java/com/example/demojap3/post/PostRepositoryTest.java
+++ src/test/java/com/example/demojap3/post/PostRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.demojap3.post;
 
+import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
@@ -15,11 +16,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
+//@Rollback(value = false)
 class PostRepositoryTest {
 
     @Autowired
     private PostRepository postRepository;
+
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Test
     public void crud(){
@@ -27,8 +32,35 @@ class PostRepositoryTest {
         post.setTitle("jpa");
         postRepository.save(post);
 
-       // List<Post> all=postRepository.findAll();
-       // Assertions.assertThat(all.size()).isEqualTo(1);
+        List<Post> all=postRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(1);
+    }
+
+
+    @Test
+    public void save(){
+        Post post=new Post();
+        post.setId(1l);
+        post.setTitle("jpa");
+        Post savedPost=postRepository.save(post); //persist
+
+        Assertions.assertThat(entityManager.contains(post)).isFalse();
+        Assertions.assertThat(entityManager.contains(savedPost)).isTrue();
+        Assertions.assertThat(savedPost==post);
+
+
+        Post postUpdate=new Post();
+        postUpdate.setId(1l);
+        postUpdate.setTitle("hibernate");
+        Post updatePost= postRepository.save(postUpdate); //update
+
+        Assertions.assertThat(entityManager.contains(updatePost)).isTrue();
+        Assertions.assertThat(entityManager.contains(postUpdate)).isFalse();
+        Assertions.assertThat(updatePost==postUpdate);
+
+
+        List<Post> all=postRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(1);
     }
 
 
